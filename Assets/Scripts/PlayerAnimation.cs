57261f52a2c982df5fbcb288eb1e22d55051f51f
Wenji <wendypu666@gmail.com>
2026-08-29 16:20:07 -0600

    Add player movement and crash animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalVelocity = rb.linearVelocityX;
+       
+        animator.SetFloat("MoveX", horizontalVelocity);
+
+        Debug.Log("MoveX: " + horizontalVelocity);
+    }
+
+    public void PlayCrash()
+    {
+        animator.SetTrigger("Crash");
+    }
+}
