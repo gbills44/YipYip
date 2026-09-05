cd9bbce92463b325ee4ce51ff002482a13f122b2
gbills44 <gbills-school@tutamail.com>
2026-09-04 20:34:32 -0600

    Remapped some player controls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isOnVoiceDash = true;
+    private bool b_gameRunning = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        b_gameRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
